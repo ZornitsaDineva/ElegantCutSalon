0f68bd959ff0b8053a8e5fdcbfec19217e4b93d3
kriskata <kriskata5000@gmail.com>
2024-12-29 11:41:38 +0200

    Endpoint Model Example

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Mvc;
+using ElegantCutSalon.Models;
+using Microsoft.AspNetCore.Antiforgery;
 namespace ElegantCutSalon
 {
     public class Program
@@ -5,36 +8,53 @@ namespace ElegantCutSalon
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
             builder.Services.AddAuthorization();
 
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
+            app.UseStaticFiles();
 
-            var summaries = new[]
-            {
-                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-            };
+            app.MapGet("/", (HttpContext httpContext) => {
+                return Results.Content("""
+                    <h1> Register Staff </h1>
+                    <form action="/staff" method="post">
+                        
+                        <lable>Name</lable>
+                        <input type="text" name="Name" placeholder="Name" required>
 
-            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    {
-                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                        TemperatureC = Random.Shared.Next(-20, 55),
-                        Summary = summaries[Random.Shared.Next(summaries.Length)]
-                    })
-                    .ToArray();
-                return forecast;
+                    <lable>Title</lable>
+                        <input type="text" name="Title" placeholder="Title" required>
+
+                    <lable>Description</lable>
+                        <input type="text" name="Description" placeholder="Description" required>
+
+                    <lable>Email</lable>
+                        <input type="email" name="Email" placeholder="Email" required>
+
+                    <lable>Role</lable>
+                        <input type="text" name="Role" placeholder="Role" required>
+
+                    <lable>Password</lable>
+                        <input type="password" name="Password" placeholder="Password" required>
+
+                        <input type="submit" value="Submit" />
+
+                    </form>
+                    """, "text/html");
             });
+
+            app.MapPost("/staff", (HttpContext httpContext, [FromForm] StaffModel s) =>
+            {
+                Console.WriteLine(s.Name);
+                Console.WriteLine(s.Title);
+                Console.WriteLine(s.Description);
+                Console.WriteLine(s.Role);
+                Console.WriteLine(s.Email);
+                Console.WriteLine(s.Password);
+            }).DisableAntiforgery();
 
             app.Run();
         }
